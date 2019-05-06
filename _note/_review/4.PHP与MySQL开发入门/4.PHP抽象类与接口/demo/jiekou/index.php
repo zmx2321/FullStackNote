050a8cd91b5666819b09eecfbab4dceebb848d23
zmx <zmx2321@163.com>
2019-05-06 15:50:05 +0800

    init

--- _note/_review/4.PHP与MySQL开发入门/4.PHP抽象类与接口/demo/jiekou/index.php
+++ _note/_review/4.PHP与MySQL开发入门/4.PHP抽象类与接口/demo/jiekou/index.php
@@ -1,46 +1,3 @@
-# PHP抽象类与接口
-### 抽象类与接口的区别
-* 接口是对动作的抽象，表示这个对象能做什么
-* 抽象类是对根源的抽象，是对一类事物的抽象描述
-
-#### 抽象类实例
-```
-<?php
-    /**
-     * 抽象类
-     * 1、含有抽象方法的类必须是抽象类
-     * 2、抽象类不一定非得含有抽象方法
-     * 3、抽象类可以存在普通方法
-     * 4、抽象类不能被实例化 必须由一个子类去继承 并且把抽象类的抽象方法都实现了
-     */
-    abstract class Person {
-        public function eat() {
-            echo "eat";
-        }
-
-        // 抽象方法没有方法体
-        public abstract function play ();
-    }
-
-    // 继承抽象类
-    class Man extends Person {
-        // 如果抽象类中有抽象方法，必须实现抽象方法
-        public function play() {
-            // TODO: Implement play() method.
-            echo "play";
-        }
-    }
-
-    // 实例化
-    $man = new Man();
-
-    // 子类就会有父类(抽象类)中的方法
-    $man -> eat();
-    $man -> play();
-```
-
-### 接口
-```
 <?php
     /**
      * 1、接口声明的关键字 interface
@@ -103,4 +60,3 @@
 
     // 通过类执行静态方法
     echo Student::test1();
-```
