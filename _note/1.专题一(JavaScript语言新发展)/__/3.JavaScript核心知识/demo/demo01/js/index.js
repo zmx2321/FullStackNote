c6605fa81648b6337efb210861b85068949abf13
yjf <627914232@qq.com>
2019-05-15 18:09:37 +0800

    init

--- _note/1.专题一(JavaScript语言新发展)/__/3.JavaScript核心知识/demo/demo01/js/index.js
+++ _note/1.专题一(JavaScript语言新发展)/__/3.JavaScript核心知识/demo/demo01/js/index.js
@@ -1,14 +1,19 @@
-/**
- * 手写一个bind
- */
-Function.prototype.bind = function (oThis) {
-    // 如果不是函数调用bind
-    // 不是对象调用bind的话
-    if (typeof this !== "function") {
-        throw new TypeError("请使用函数执行");
+if (!Function.prototype.bind) {
+    Function.prototype.bind = function (oThis) {
+        if (typeof this !== "function") {
+            throw new TypeError("请使用函数执行");
+        }
+        var aArgs = Array.prototype.slice.call(arguments, 1),
+            fToBind = this,
+            fNop = function () { },
+            fBound = function () {
+                //this
+                return fToBind.apply(this instanceof fBound ? this : oThis, aArgs.concat(Array.prototype.slice.call(arguments)));
+            }
+        if (this.prototype) {
+            fNop.prototype = this.prototype;
+        }
+        fBound.prototype = new fNop();
+        return fBound;
     }
-
-    // 把外面的参数拿过来
-    var aArgs = Array.prototype.slice.call(arguments, 1);
-    fToBind = this;
 }
