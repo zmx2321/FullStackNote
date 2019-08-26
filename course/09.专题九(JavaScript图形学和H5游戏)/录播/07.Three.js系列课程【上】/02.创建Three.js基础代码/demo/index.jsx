3f5e5a462ab38f783af65341cfc0471672a157cb
zmx2321 <zmx2321@163.com>
2019-08-26 23:28:56 +0800

    init

---
+++ course/09.专题九(JavaScript图形学和H5游戏)/录播/07.Three.js系列课程【上】/02.创建Three.js基础代码/demo/index.jsx
@@ -0,0 +1,11 @@
+const React = require("react");
+
+let index = React.createClass({
+    render: function() {
+        return (
+            <div className="test">第一个React</div>
+        );
+    }
+});
+
+module.export = index;
